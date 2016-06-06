7022ba58fbb3faca0743d32bbb8628dd5d8e85e9
Joshua May <notjosh@gmail.com>
2016-06-07 01:37:48 +1000

    Initial Commit

---
+++ NTJDominantColours/NTJDominantColours.h
@@ -0,0 +1,19 @@
+//
+//  NTJDominantColours.h
+//  NTJDominantColours
+//
+//  Created by joshua may on 7/06/2016.
+//  Copyright © 2016 joshua may. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NTJDominantColours.
+FOUNDATION_EXPORT double NTJDominantColoursVersionNumber;
+
+//! Project version string for NTJDominantColours.
+FOUNDATION_EXPORT const unsigned char NTJDominantColoursVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NTJDominantColours/PublicHeader.h>
+
+
